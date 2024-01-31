3c202d3063c3e7698f4d2f99105479c15e818b88
Amirani <amiraniburjanadze@gmail.com>
2024-01-31 21:41:02 +0400

    change map to mymap

--- myMap.js
+++ myMap.js
@@ -1,9 +1,10 @@
 /*
-იმისათვის რომ Array.map - ის კლონი შევქმნათ საჭიროა შემდეგი ნაბიჭები
 
-  1) ვქმნით ახალი მეთოდს Array prototype - ში, როგორც ვიცით Constructor ფუნქციებს გააჩნიათ prototype ობიექტი რომელში არსებული მეთოდების გამოყენებაც მის instance - ებს შეუძლიათ, ჩვენს შემთხვევაში Array instance - იქნება  ნებისმიერი Array რასაც შევქმნით მაგალითად: let cars = []
-  2) map - ის გამოძახების დროს არგუმენტად გადავცემთ ფუნქციას ამიტომ MyMap - ის შემთხვევაშიც ასე უნდა მოვიქცეთ, გადაცემული ფუნქცის გამოძახება უნდა მოხდეს ყოველი Array item - ის იტერაციისას სამი არგუმენტით: elemeny,index,მთლიანი array
+იმისათვის რომ Array.map - ის კლონი შევქმნათ საჭიროა შემდეგი ნაბიჯები:
+  1) ვქმნით ახალი მეთოდს Array prototype - ში, როგორც ვიცით Constructor ფუნქციებს გააჩნიათ prototype ობიექტი რომელში არსებული მეთოდების გამოყენებაც მის instance - ებს შეუძლიათ, ჩვენს შემთხვევაში Array instance იქნება  ნებისმიერი Array რომესლაც შევქმნით მაგალითად: let cars = []
+  2) map - ის გამოძახების დროს არგუმენტად გადავცემთ ფუნქციას, MyMap - ის შემთხვევაშიც ასე უნდა მოვიქცეთ, გადაცემული ფუნქცის გამოძახება უნდა მოხდეს ყოველი Array item - ის იტერაციისას სამი არგუმენტით: elemeny,index,მთლიანი array
   3) საბოლოოდ ჩვენ დავაბრუნებთ ახალ Array - ის.
+  
 */
 
 Array.prototype.mymap = function(callback) {
@@ -11,12 +12,12 @@ Array.prototype.mymap = function(callback) {
     // ვქმნით ახალ მასივს რადგან ვიცით რომ როგორც map - მა შეცვლილი ვერსია უნდა დავაბრუნოთ.
     for (let index = 0; index < this.length; index++) {
         /* 
-          ვიყენებთ For ლოოპს რათა Array - ის აითემებზზე მოვახდინოთ იტერაცია ამისთვის ვიყენეთ 
-          this keyword - ს, ვიცით რომ როცა ფუნქციას ვიძახებთ, ჩველებრივ this = იმ ობიექტს   
+          ვიყენებთ For ლუპს რათა Array - ის აითემებზე მოვახდინოთ იტერაცია, ამისთვის ვიყენეთ 
+          this keyword - ს, ვიცით რომ როცა ფუნქციას ვიძახებთ, ჩველებრივ this უდრის იმ ობიექტს   
           რომლის მეშვეობითაც სრულდება ფუნქციის გამოძახება.
         */
         resultArray.push(callback(this[index], index, this));
-        // აქ კი ახალ ერეიში ვინახავთ იმ Value - ს რასაც ჩვენი Callback მეთოდი დაგვიბრუნებს
+        // აქ კი ახალ ერეიში ვინახავთ იმ Value - ს რასაც ჩვენი Callback ფუნქცია დაგვიბრუნებს
     }
     return resultArray;
     // საბოლოოდ კი ვაბრუნებთ ახალ მასივს.
@@ -26,9 +27,9 @@ const numbers = [1,2,3];
 const scaleNumbersWithMap = numbers.map((number, index, array) => {
   return number * 2
 })
-const scaleNumbersWithMyMap = numbers.map((number, index, array) => {
+const scaleNumbersWithMyMap = numbers.mymap ((number, index, array) => {
   return number * 2
 })
 
 console.log(scaleNumbersWithMap , scaleNumbersWithMyMap );
-// ორივე მნიშვნელობა დაგვიბრუნებს ერთი და იგივეს.
+// ორივე მნიშვნელობა დააბრუნებს ერთი და იგივეს.
